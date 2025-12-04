f66e90a340cfffb7fedb46c98bce79ba7f6502ea
kankodu <reflectivecamouflage@gmail.com>
2025-12-04 14:38:13 +0530

    fix: forge fmt

--- src/ERC721WrapperBase.sol
+++ src/ERC721WrapperBase.sol
@@ -2,9 +2,7 @@
 pragma solidity 0.8.26;
 
 import {IERC6909, ERC6909} from "lib/openzeppelin-contracts/contracts/token/ERC6909/ERC6909.sol";
-import {
-    ERC6909TokenSupply
-} from "lib/openzeppelin-contracts/contracts/token/ERC6909/extensions/ERC6909TokenSupply.sol";
+import {ERC6909TokenSupply} from "lib/openzeppelin-contracts/contracts/token/ERC6909/extensions/ERC6909TokenSupply.sol";
 import {EnumerableSet} from "lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
 import {Context} from "lib/openzeppelin-contracts/contracts/utils/Context.sol";
 import {IERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
